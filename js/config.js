0182d0cfaa0a5df0596d73300837dfeddc37a18e
fajar-dev <fajarrivaldi2015@gmail.com>
2026-04-29 15:15:53 +0700

    chore: config and crx file

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = Object.freeze({
-  BASE_API:        'http://localhost:4000/api',
+  BASE_API:        'https://transit.is5x.nusa.net.id/coretax-sync',
   CORETAX_API:      'https://coretaxdjp.pajak.go.id/documentmanagementportal/api',
   CORETAX_HOST:     'pajak.go.id',
   STORAGE_KEY_USER: 'userInfo',
